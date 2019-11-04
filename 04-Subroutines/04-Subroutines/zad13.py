f0ef10153655a089abf852e190a68630367bc01d
JanaszekM <56255733+JanaszekM@users.noreply.github.com>
2019-11-04 12:59:09 +0100

    Add files via upload

---
+++ 04-Subroutines/04-Subroutines/zad13.py
@@ -0,0 +1,8 @@
+
+tab=[4,3,7,1,3]
+def suma_tablicy():
+    x=sum(tab)
+    print(x)
+    
+    
+
---
+++ 04-Subroutines/04-Subroutines/zadpierwsze.py
@@ -0,0 +1,11 @@
+
+def num():
+    for i in range(1,10):
+        if i==3 or i==6:
+            
+            print(i)
+            
+        else:
+            print(i, end=' ')
+
+
